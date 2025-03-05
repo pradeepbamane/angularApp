a0571f0330670e0b838a5c49d26d99e75f5d60ec
pradeepbamane <bamane.pradeep3@gmail.com>
2025-03-05 10:36:26 +0530

    complete button

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -41,4 +41,7 @@ export class TasksComponent {
   get selectedUserTasks(){
     return this.tasks.filter((task=>task.userId===this.userId));
   }
+  onCompleteTask(id:string){
+    this.tasks = this.tasks.filter((task)=>task.id!==id);
+  }
 }
